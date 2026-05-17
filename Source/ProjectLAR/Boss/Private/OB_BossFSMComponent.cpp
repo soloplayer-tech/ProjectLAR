358f7177ae97264ec295c00d6bd80dffc7bbb3f3
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-05-18 00:30:30 +0900

    FSM Define
    - 상태 정의 및 초기 구현
    - TODO에 있는 세부 기능들 구현 필요

---
+++ Source/ProjectLAR/Boss/Private/OB_BossFSMComponent.cpp
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OB_BossFSMComponent.h"
+
+
+// Sets default values for this component's properties
+UOB_BossFSMComponent::UOB_BossFSMComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	
+	PrimaryComponentTick.bCanEverTick = false; // 틱 계산 X, 이벤트 단위로 계산 진행
+
+	// ...
+}
+
+// Called when the game starts
+void UOB_BossFSMComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+// Called every frame
+void UOB_BossFSMComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                         FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+void UOB_BossFSMComponent::SetState(EBossState NewState)
+{
+	if (CurAIState == NewState) return;
+		// OnExitState(CurAIState); // TODO: ExitState로 상태 해제 관리할 요소가 없어 주석 -> 향후 추가 시 내용 재정의
+		CurAIState = NewState;
+		OnEnterState(CurAIState);
+}
+
+// 상태 진입 시 행동 정의 
+void UOB_BossFSMComponent::OnEnterState(EBossState BossState)
+{	
+	/* TODO :
+	 * 각 상태별 행동 정의 
+	 */
+	
+	UE_LOG(LogTemp, Display, TEXT("[ Enter State : %s ]"), *UEnum::GetValueAsString(BossState));
+	
+	switch (BossState)
+	{
+	case EBossState::	IDLE:		break;
+	case EBossState::	MOVE:		break;
+	case EBossState::	ATTACK:		break;
+	case EBossState::	STUNNED:	break;
+	case EBossState::	DAMAGE:		break;
+	case EBossState::	DIE:		break;
+	}
+}
+
---
+++ Source/ProjectLAR/Boss/Public/OB_BossFSMComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "States/OB_EBossAIState.h"
+#include "OB_BossFSMComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTLAR_API UOB_BossFSMComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UOB_BossFSMComponent();
+	
+	// 보스 열겨형 호출
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BossFSM")
+	EBossState CurAIState = EBossState::IDLE;						// 현재 상태 저장 변수
+	
+
+	void SetState(EBossState NewState);								// 상태 변경
+	void OnEnterState(EBossState BossState);						// 상태 ENTER 정의
+	// void OnExitState(EBossState BossState);							// TODO: 상태 EXIT 정의 현재 개발 단계상 불필요
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
