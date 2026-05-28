89d0b96e9e968c3d9eaf5a1a5d817cd96654e3e4
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-05-28 10:16:33 +0900

    Update OB_CombatComponent.h
    - Combat 변수들 미리 정의

--- Source/ProjectLAR/Boss/Public/OB_CombatComponent.h
+++ Source/ProjectLAR/Boss/Public/OB_CombatComponent.h
@@ -23,10 +23,29 @@ class PROJECTLAR_API UOB_CombatComponent : public UActorComponent
 public:
 	// Sets default values for this component's properties
 	UOB_CombatComponent();
+	
+	// TODO: EAttackState GetFitAttackPattern(float Distance); 추후 디테일 작업들어갈 떄 개발
+	
+	void TakeDamage(float DamageAmount);
+	float GetCurHP() const { return CurHP };
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, Category="Boss | Stats")
+	float MaxHP = 1000.0f;
+	
+	float CurHP;
+	
+	// RUSH 전술 수치
+	UPROPERTY(EditAnywhere, Category = "Boss | Tactics")
+	float RushTriggerDistance = 600.0f; // 6미터 이상이면 돌진
+
+	// RUSH 연속 발동을 막기 위한 내부 쿨타임 제어 변수
+	bool bIsRushCooldown = false;
+	FTimerHandle RushCooldownTimerHandle;
+	
 
 public:
 	// Called every frame
