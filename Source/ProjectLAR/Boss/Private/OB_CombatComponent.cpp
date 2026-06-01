fb1545c2ea4fb271687a6572e40e7b03e46360ac
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-06-01 18:29:46 +0900

    Level Design Complete
    - 레벨디자인을 완료
    - 추가 공유사항은 #7 LevelDesign 공유내용 참고

--- Source/ProjectLAR/Boss/Private/OB_CombatComponent.cpp
+++ Source/ProjectLAR/Boss/Private/OB_CombatComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "OB_CombatComponent.h"
+#include "OB_BossCharacter.h"
+#include "OB_BossFSMComponent.h"
 
 
 // Sets default values for this component's properties
@@ -9,22 +11,97 @@ UOB_CombatComponent::UOB_CombatComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 	CurHP = MaxHP;
+	
+	OwnerCharacter = Cast<AOB_BossCharacter>(GetOwner());
+	if (OwnerCharacter != nullptr)
+	{
+		PatternComp = OwnerCharacter -> GetPatternComponent();
+	}
+}
+
+void UOB_CombatComponent::StartAttack()
+{
+	LOG_TRACE_INFO("Call StartAttack")
+	
+	if (!bCanAttack)
+	{
+		LOG_TRACE_INFO("Is CoolTime");
+		return;
+	}
+	
+	if (!PatternComp)
+	{
+		LOG_TRACE_WARN("PatternComp is null!!");
+		return;
+	}
+	
+	PatternComp -> SelectAndExecute();
+	
+	bCanAttack = false;
+	GetWorld() -> GetTimerManager().SetTimer(
+		CooldownTimer,
+		this,
+		&UOB_CombatComponent::OnCooldownFinished,
+		AttackCooldown,
+		false
+	);
+	
+}
+
+void UOB_CombatComponent::StopAttack()
+{
+	LOG_TRACE_INFO("Call StopAttack");
+	
+	GetWorld() -> GetTimerManager().ClearTimer(CooldownTimer);
+	bCanAttack = true;
 }
 
 void UOB_CombatComponent::TakeDamage(float DamageAmount)
 {
-	CurHP = FMath::Max(0.f, CurHP - DamageAmount);
+	if (IsDead()) return;
 	
-	if (CurHP <= 0.0f)
+	CurHP = FMath::Clamp( CurHP - DamageAmount , 0.f, MaxHP );
+	
+	LOG_TRACE_INFO(TEXT("[Combat] HP : %.0f / %.0f (%.0f%%)"), CurHP, MaxHP, GetHPRatio() * 100.0f);
+	
+	// TODO : Phase2 진입시 ( HP <= 50% )
+	
+	if (IsDead()) OnDead();
+}
+
+
+void UOB_CombatComponent::OnCooldownFinished()
+{
+	bCanAttack = true;
+	LOG_TRACE_INFO("CoolTime is Finished");
+	
+	if ( OwnerCharacter )
 	{
-		// TODO : Die 상태로 변경하는 로직
+		auto* FSMComp = OwnerCharacter -> GetFSMComponent();
+		if ( FSMComp && FSMComp -> GetCurState()==EBossBattleState::ATTACK)
+		{
+			PatternComp -> SelectAndExecute();
+		}
 	}
 }
 
+void UOB_CombatComponent::OnDead()
+{
+	LOG_TRACE_WARN("[Combat] Boss is Dead");
+	
+	if ( OwnerCharacter )
+	{
+		auto* FSMComp = OwnerCharacter->GetFSMComponent();
+		
+		if (FSMComp)
+			FSMComp->SetState(EBossBattleState::DEAD);
+	}
+	
+}
 
 // Called when the game starts
 void UOB_CombatComponent::BeginPlay()
